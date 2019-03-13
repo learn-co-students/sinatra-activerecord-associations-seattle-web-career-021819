ebf1e33cb779b4bb044eff3cc059838cbab85de4
github username <github email address>
2019-03-13 16:40:13 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
 end
+
+
+Cat.create(:name => "Maru", :age => 3, :breed => "Scottish Fold")
+Cat.create(:name => "Hannah", :age => 2, :breed => "Tabby")
+Cat.create(:name => "Patches", :age => 2, :breed => "Calico")
+Owner.create(:name => "Sophie")
+Owner.create(:name => "Ann")
